840dc27201270923053a48c132d7a7da9d09c5ee
Yann Gouffon <yago@yago-art.ch>
2015-02-07 14:45:24 +0100

    rename client to public

---
+++ public/app/admin/admin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('cleganeApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('admin', {
+        url: '/admin',
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
