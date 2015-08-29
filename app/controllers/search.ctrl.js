350669f6799c5f7b116aa127486fd2cc60591ac5
Yann Gouffon <github@yago-art.ch>
2015-08-29 17:52:41 +0200

    add tag page

--- app/controllers/search.ctrl.js
+++ app/controllers/search.ctrl.js
@@ -43,14 +43,48 @@ exports.genres = function(req, res) {
 
             res.locals.data = data;
             res.locals.query = genreId;
-            res.locals.type = 'genres';
+            res.locals.type = 'genre';
             res.locals.genre = genre;
             res.render('results');
 
           }, function (err) {
             res.locals.data = data;
             res.locals.query = genreId;
-            res.locals.type = 'genres';
+            res.locals.type = 'genre';
+            res.render('results');
+          });
+
+    }, function (err) {
+      res.send('No result founded');
+    });
+};
+
+/*
+ * Display tag's results
+ */
+exports.tags = function(req, res) {
+  var userId = req.body.userId,
+      tagId = req.params.id,
+      page = req.params.page;
+
+  // Request tag's results
+  apiCtrl.getpage('/keyword/'+tagId+'/movies', page,
+    function (data) {
+
+      // Request tag's infos
+        apiCtrl.get('/keyword/'+tagId,
+          function (tag) {
+
+            res.locals.data = data;
+            res.locals.query = tagId;
+            res.locals.type = 'tag';
+            res.locals.tag = tag;
+            res.render('results');
+
+          }, function (err) {
+            res.locals.data = data;
+            res.locals.query = tagId;
+            res.locals.type = 'tag';
             res.render('results');
           });
 
--- app/routes/search.routes.js
+++ app/routes/search.routes.js
@@ -6,6 +6,7 @@ var searchCtrl    = require('../controllers/search.ctrl'),
 module.exports = function(app, passport) {
 
   app.get('/search/:query/:page', auth.isAuthenticated, searchCtrl.results);
-  app.get('/genres/:id/:page', auth.isAuthenticated, searchCtrl.genres);
+  app.get('/genre/:id/:page', auth.isAuthenticated, searchCtrl.genres);
+  app.get('/tag/:id/:page', auth.isAuthenticated, searchCtrl.tags);
 
 };
