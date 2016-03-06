93b46b4e2dc6549b800020dec09402292078af8a
Yann Gouffon <github@yago-art.ch>
2016-03-06 16:45:23 +0100

    #19 don't use rsz.io proxy any more and change images width based on api.themoviedb.org/3/configuration

--- assets/app/filters.js
+++ assets/app/filters.js
@@ -69,19 +69,11 @@ app.filter('rowArray', function() {
 app.filter('resize', function() {
   return function(image, width, ratio, gravity) {
     if (image) {
-      var picture = 'https://image.tmdb.org/t/p/original'+image;
+      var picture = 'https://image.tmdb.org/t/p/'+width+image;
     } else {
       var picture = 'http://i.imgur.com/FOKzIc4.png';
     }
-
-    var height  = Math.round(parseFloat(width)/parseFloat(ratio)),
-        baseUrl = picture.replace('https', 'http').replace('.tmdb.org', '.tmdb.org.rsz.io').replace('.imgur.com', '.imgur.com.rsz.io');
-
-    if (width <= 1600 && height <= 1600) {
-      return baseUrl+'?width='+width+'&height='+height+'&mode=crop&anchor='+gravity;
-    } else {
-      return picture;
-    }
+    return picture;
   };
 });
 
