8e5888261133325b66e4a86b0ae643c46cbc19eb
Yann Gouffon <github@yago-art.ch>
2016-06-27 10:28:52 +0200

    sidebar nav

--- app/components/Sidebar/Sidebar.jsx
+++ app/components/Sidebar/Sidebar.jsx
@@ -14,7 +14,45 @@ class Sidebar extends React.Component {
           <Link to={'/'} className="brand">
             <div className="icon icon-clegane"></div>
           </Link>
+
           <Search />
+
+          <div className="sidebar-nav">
+            <Link to={'#'} className="link-iconed">
+              <i className="icon icon-gauge"></i> Dashboard
+            </Link>
+
+            <h5>Discover</h5>
+            <Link to={'#'} className="link-iconed">
+              <i className="icon icon-film"></i> Movies
+            </Link>
+            <Link to={'#'} className="link-iconed">
+              <i className="icon icon-television"></i> TV Shows
+            </Link>
+            <Link to={'#'} className="link-iconed">
+              <i className="icon icon-oscar"></i> People
+            </Link>
+
+            <h5>Personnal</h5>
+            <Link to={'#'} className="link-iconed">
+              <i className="icon icon-eye"></i> Watched
+            </Link>
+            <Link to={'#'} className="link-iconed">
+              <i className="icon icon-star"></i> My lists
+            </Link>
+            <Link to={'#'} className="link-iconed">
+              <i className="icon icon-cog"></i> Settings
+            </Link>
+            <Link to={'#'} className="link-iconed">
+              <i className="icon icon-out"></i> Logout
+            </Link>
+
+            <h5>Clegane</h5>
+            <Link to={'#'} className="link-iconed">
+              <i className="icon icon-info"></i> About
+            </Link>
+          </div>
+
         </div>
       </div>
     );
