e939d1fdd2ecfdb11b0ab85be07e16851db40dbb
Yann Gouffon <github@yago-art.ch>
2019-09-03 20:25:28 +0200

    ✨ add TMDB base service

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,14 @@
 /** @jsx jsx */
-import React, {} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { jsx } from '@emotion/core'; // eslint-disable-line
 
+import { fetchTV } from 'services/tmdb';
 import styles from './App.styles';
 
-const App = ({  }) => {
-  return (
-    <div css={styles}>
-      Sup ?
-    </div>
-  );
+const App = ({}) => {
+  fetchTV(1432).then(res => console.log(res));
+  return <div css={styles}>Sup ?</div>;
 };
 
 App.propTypes = {};
---
+++ src/services/tmdb.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import { pipe } from 'ramda';
+
+// Append key to API URL
+const useKey = key => url =>
+  `${url}${url.includes('?') ? '&' : '?'}api_key=${key}`;
+
+// Axios fetch wrapper
+const fetch = url =>
+  axios
+    .get(url)
+    .then(res => res.data)
+    .catch(err => console.error('Damned...', err));
+
+// Based on URL, execute required tasks to fetch data
+const composeApi = pipe(
+  useKey('6d83177ea3e67b870ab80fa72f06cbbd'),
+  fetch
+);
+
+// Construct the full API URL
+const fetchApi = baseURL => endpoint => composeApi(`${baseURL}${endpoint}`);
+
+// Set TMDB base url
+const fetchTMDB = fetchApi('http://api.themoviedb.org/3');
+
+// Specific endpoints
+export const fetchTV = id => fetchTMDB(`/tv/${id}`);
